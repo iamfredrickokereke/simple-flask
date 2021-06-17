8d281be040c718151aab9ae69e72bfdad286ce2e
Kelly online <kellyonline2016@gmail.com>
2021-06-17 19:16:56 +0100

    updated with a new category route

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
     return 'Welcome to my simple blog!'
+
+
+@app.route('/api/blog/categories')
+def categories():
+    return 'This is the category section!'
