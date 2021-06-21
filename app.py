1588dddc2b9b1b0764e9314d254839774afde9a6
Kelly online <kellyonline2016@gmail.com>
2021-06-21 12:58:13 +0100

    created docker file and updated content

--- app.py
+++ app.py
@@ -4,6 +4,40 @@ from flask import render_template
 app = Flask(__name__)
 
 
+posts = [
+    {
+        'title': 'Devops and Cloud Native',
+        'author': 'Dare',
+        'day': 'day one',
+        'content': 'Onboarding and installing applications in lesson one',
+        'date_posted': 'June 15, 2021'
+    },
+    {
+        'title': 'Devops and Cloud Native',
+        'author': 'Fredrick',
+        'day': 'day two',
+        'content': 'Implementing Lesson One',
+        'date_posted': 'June 16, 2021'
+    },
+    {
+        'title': 'Devops and Cloud Native',
+        'author': 'Kelly',
+        'day': 'day three',
+        'content': 'Working on Demo and implementing Lesson two',
+        'date_posted': 'June 17, 2021'
+    },
+    {
+        'title': 'Devops and Cloud Native',
+        'author': 'Achiever',
+        'day': 'day four',
+        'content': 'Revising lesson two and improving flask project',
+        'date_posted': 'June 18, 2021'
+    }
+
+
+]
+
+
 @app.route('/')
 @app.route('/home')
 def home():
@@ -20,10 +54,10 @@ def categories():
     return 'This is the category section!'
 
 
-@app.route('/api/blog/day1')
-def dayone():
-    return render_template('index.html')
+@app.route('/api/blog/daily')
+def daily():
+    return render_template('index.html', posts=posts)
 
 
 # if __name__ == "__main__":
-#     app.run(debug=True, port=9000)
+#     app.run(debug=True, port=9000, host='0.0.0.0')
